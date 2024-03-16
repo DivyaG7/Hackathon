0c3f36662a35fc680cca29bacedcb7c4e1b802bd
unknown <divyaneela75@gmail.com>
2024-03-16 21:22:08 +0530

    Initial commit

--- hackathon.js
+++ hackathon.js
@@ -14,3 +14,23 @@ const observer = new IntersectionObserver(handleAnimation, { threshold: 0.5 });
 document.querySelectorAll('.animated-section').forEach(section => {
     observer.observe(section);
 });
+
+
+// Show the button when user scrolls down 20px from the top of the document
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+    }
+}
+
+// Scroll to the top when the button is clicked
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
